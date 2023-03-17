72802f1256cca09be697c7219f132df9f1c9c4a9
Andres <ricardo.gomez.restrepo@gmail.com>
2023-03-17 12:01:59 -0500

    new styles applied

--- todo-list/src/components/Items/index.tsx
+++ todo-list/src/components/Items/index.tsx
@@ -18,9 +18,11 @@ export default function Items() {
   if (isError) return <Error message={`Got error ${error}`} />;
   return (
     <>
-      <div>
+      <div className="mx-auto yPaddings lg:w-[650px] w-[600px]  h-auto flex flex-col gap-[14px]">
         {data &&
-          data.map((item: Data) => <Card title={item.title} key={item.id} />)}
+          data.map((item: Data) => (
+            <Card title={item.title} body={item.body} key={item.id} />
+          ))}
       </div>
     </>
   );
