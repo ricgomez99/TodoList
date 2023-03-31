56a3512dc3ba7a526b986ddb8d1ecd79c01a4b01
Andres <ricardo.gomez.restrepo@gmail.com>
2023-03-31 17:10:18 -0500

    cards animation applied

--- todo-list/src/components/Items/index.tsx
+++ todo-list/src/components/Items/index.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { motion } from "framer-motion";
+import { staggerContainer } from "@/utils/motion";
 import { useQuery } from "react-query";
 import { getItems } from "@/lib/helper";
 import Error from "../Error";
@@ -19,17 +21,24 @@ export default function Items() {
   if (isError) return <Error message={`Got error ${error}`} />;
   return (
     <>
-      <div className="mx-auto yPaddings lg:w-[650px] w-[600px]  h-auto flex flex-col gap-[14px]">
+      <motion.div
+        variants={staggerContainer as any}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className="mx-auto yPaddings lg:w-[650px] w-[600px]  h-auto flex flex-col gap-[14px]"
+      >
         {data &&
-          data.map((item: Data) => (
+          data.map((item: Data, index: number) => (
             <Card
               title={item.title}
               body={item.body}
               id={item._id}
               key={item._id}
+              index={index}
             />
           ))}
-      </div>
+      </motion.div>
     </>
   );
 }
